76af9bf2108dea84a60e544a994ba293bf41912c
bornkiller <hjj491229492@hotmail.com>
2016-01-21 18:28:34 +0800

    add sonar sub module test

---
+++ test/sonar.spec.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const should = require('should');
+const sonar = require('../base/sonar.js');
+const template = fs.readFileSync(path.join(__dirname, 'fixture', 'index-origin.html')).toString();
+
+describe('sonar sub module', function () {
+  it('should return html tag when template valid', function () {
+    sonar.seekHtmlTag(template).trim().should.equal('<html lang="zh-CN">')
+  });
+
+  it('should return false when template invalid', function () {
+    sonar.seekHtmlTag('<p class="text-info"></p>').should.be.false;
+  });
+
+  it('should return body tag when template valid', function () {
+    sonar.seekBodyTag(template).trim().should.equal('<body class="container">')
+  });
+
+  it('should return body tag when template invalid', function () {
+    sonar.seekBodyTag('<p class="text-info"></p>').should.be.false;
+  });
+
+  it('should return false when bodyTag have nothing class attribution', function () {
+    sonar.seekClassAttr('<body id="world">').should.be.false;
+  });
+
+  it('should return class name when bodyTag have class attribution', function () {
+    sonar.seekClassAttr('<body class="text-info">').trim().should.equal('class="text-info"')
+  });
+
+  it('should generate h5 class when zero argument', function () {
+    sonar.seekCordovaClass().trim().should.equal('platform-h5');
+  });
+
+  it('should generate related cordova class', function () {
+    sonar.seekCordovaClass('ios').trim().should.equal('platform-ios platform-cordova platform-webview');
+    sonar.seekCordovaClass('android').trim().should.equal('platform-android platform-cordova platform-webview');
+  });
+});
